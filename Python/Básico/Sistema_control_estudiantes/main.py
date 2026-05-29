9d639964fb26412df24a13c9add904d2faaa5afd
Luis Felipe Brenes <luisfelipebrenes@gmail.com>
2026-05-28 19:21:26 -0600

    feat: add main.py and menu.py

---
+++ Python/Básico/Sistema_control_estudiantes/main.py
@@ -0,0 +1,8 @@
+import menu
+
+
+def main():
+    menu()
+
+
+main()
---
+++ Python/Básico/Sistema_control_estudiantes/menu.py
@@ -0,0 +1,38 @@
+from actions import *
+from data import *
+
+
+def menu():
+    while True:
+        print("Sistema de control de  estudiantes")
+        print("1. Registrar estudiante")
+        print("2. Ver todos los estudiantes")
+        print("3. Ver top 3 estudiantes")
+        print("4. Ver Promedio general")
+        print("5. Exportar datos")
+        print("6. Importar datos")
+        print("7. Salir")
+
+        try:
+            user_option = int(input("Elija una opcion del menu: "))
+
+            if user_option == 1:
+                register_student()
+            elif user_option == 2:
+                display_all_students()
+            elif user_option == 3:
+                display_top_3()
+            elif user_option == 4:
+                display_general_average()
+            elif user_option == 5:
+                export_students_data()
+            elif user_option == 6:
+                import_students_data()
+            elif user_option == 7:
+                break
+            else:
+                print("Seleccionaste un numero invalido.")
+
+        except ValueError as ex:
+            print(
+                f"Tienes que elegir un numero entero valido. Detalles: {ex}")
