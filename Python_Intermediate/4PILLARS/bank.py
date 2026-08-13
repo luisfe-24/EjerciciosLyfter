8dd1e1972fda07d597d078d0dcab64295efe98e5
Luis Felipe Brenes <luisfelipebrenes@gmail.com>
2026-08-12 20:09:19 -0600

    feat: bank exercise

---
+++ Python_Intermediate/4PILLARS/bank.py
@@ -0,0 +1,33 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit_money(self, amount):
+        self.balance += amount
+        print("Monto ingresado con exito")
+
+    def withdraw_money(self, amount):
+        self.balance -= amount
+        print("Monto retirado con exito")
+
+
+class SavingsAccounts(BankAccount):
+    def __init__(self, balance, min_balance):
+        super().__init__(balance)
+        self.min_balance = min_balance
+
+    def withdraw_money(self, amount):
+
+        if (self.balance - amount) < self.min_balance:
+            raise ValueError("No puede retirar: supera el monto minimo")
+
+        super().withdraw_money(amount)
+
+
+my_account = SavingsAccounts(10, 5)
+
+my_account.withdraw_money(5)
+
+my_account.deposit_money(10)
+
+my_account.withdraw_money(11)
