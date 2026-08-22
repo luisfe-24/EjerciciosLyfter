168ef3cd6b09f24087b23c1c348bef60bf378b65
Luis Felipe Brenes <luisfelipebrenes@gmail.com>
2026-08-21 19:56:01 -0600

    Extra exercises

---
+++ Python_Intermediate/Decorators_Extra/multiply.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+
+def validate_numbers(func):
+    def wrapper(*args, **kwargs):
+        all_values = list(args) + list(kwargs.values())
+        for value in all_values:
+            if type(value) not in (int, float):
+                raise ValueError("El dato ingresado no es un numero")
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def log_call(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        print(
+            f"func:{func.__name__} - args: {args} - [{datetime.now()}] -"f" Resultado: {result}")
+
+        print(f"Resultado {result}")
+        return result
+    return wrapper
+
+
+@validate_numbers
+@log_call
+def multiply(a, b):
+    return (a*b)
+
+
+multiply(3, 4)
